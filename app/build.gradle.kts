48d035c01454108f01bb7ed6cb66a3cc92801361
Priyanshu Kumar <1priyanshukumar290404@gmail.com>
2026-09-12 00:38:06 +0530

    chore(release): publish v1.5.2 (Build 10502)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.milo.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10501
-        versionName = "1.5.1"
+        versionCode = 10502
+        versionName = "1.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
